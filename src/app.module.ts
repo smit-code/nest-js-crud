c6e5c0effdedb737611564e47f2fb47c12d4accb
axit vadi <axitvadi@gmail.com>
2022-08-16 18:54:44 +0530

    feat: student module
    Co-Authored-By: Smit Gajera <smitgajera6@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { StudentsModule } from './students/students.module';
 import { Student, StudentSchema } from '../schema/student.schema';
-import { StudentsService } from '../src/students/students.service';
+import { StudentsController } from './students/students.controller';
+import { StudentsService } from 'src/students/students.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest'),
     MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema }]),
-    StudentsModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, StudentsController],
   providers: [AppService, StudentsService],
 })
 export class AppModule {}
